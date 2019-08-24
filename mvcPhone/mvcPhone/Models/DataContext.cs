431a249403bdee40d32a260bdab36c6ccc1e5477
ivandmg150@gmail.com <ivandmg150@gmail.com>
2019-08-24 18:45:12 -0300

    Solucion Completa

---
+++ mvcPhone/mvcPhone/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace mvcPhone.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext: DbContext
+    {
+        public DataContext(): base ("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<mvcPhone.Models.Phone> Phones { get; set; }
+    }
+}
